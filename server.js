ca9e055607c760978bacbd0a9419d6e543851b8e
Meni <menil@MENI.com>
2024-09-23 11:36:11 +0300

    get from id

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-import express from 'express'
-import fs from 'fs/promises'
+import express from "express";
+import fs from "fs/promises";
 
-const app = express()
+const app = express();
 
-const port = 7499
+const port = 7499;
 
-app.get('/', (req,res)=>{
-    res.send("welcome to my amnunition Center")
-})
+app.get("/", (req, res) => {
+  res.send("welcome to my amnunition Center");
+});
 
-app.get('/amn',async (req,res)=>{
-    try{
-            //get the data from file
-    const data = JSON.parse (await fs.readFile('./data.json','utf-8'))
+app.get("/amn", async (req, res) => {
+  try {
+    //get the data from file
+    const data = JSON.parse(await fs.readFile("./data.json", "utf-8"));
     //sent it to the client
-    res.json(data)
-    } catch(err){
-        res.status(500).json({
-            err:true,
-            message: err
-        })
-    }
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      err: true,
+      message: err,
+    });
+  }
+});
 
-})
+app.get("/amn/:id", async (req, res) => {
+  try {
+    //get data from file
+    const data = JSON.parse(await fs.readFile("./data.json", "utf8"));
+    //find the one with right id
+    const amn = data.find((am) => am.id === req.params.id);
+    //send it back to the client
+    res.json(amn);
+  } catch (err) {
+    res.status(500).json({
+      err: true,
+      message: err,
+    });
+  }
+});
 
-app.get('/amn/:id',(req,res)=>{
-    res.send("welcome to my amnunition Center")
-})
+app.get("/amn/summary", (req, res) => {
+  res.send("welcome to my amnunition Center");
+});
 
-app.get('/amn/summary',(req,res)=>{
-    res.send("welcome to my amnunition Center")
-})
+app.post("/amn", (req, res) => {
+  res.send("welcome to my amnunition Center");
+});
 
-app.post('/amn',(req,res)=>{
-    res.send("welcome to my amnunition Center")
-})
+app.patch("/amn/:id", (req, res) => {
+  res.send("welcome to my amnunition Center");
+});
 
-app.patch('/amn/:id',(req,res)=>{
-    res.send("welcome to my amnunition Center")
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`server started on port: ${port} visit http://localhost:${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`server started on port: ${port} visit http://localhost:${port}`);
+});
