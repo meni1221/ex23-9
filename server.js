c8b23117ba1b3398a97a4a8cfa2c286eddb51818
Meni <menil@MENI.com>
2024-09-23 11:23:28 +0300

    get all ep

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 import express from 'express'
+import fs from 'fs/promises'
 
 const app = express()
 
 const port = 7499
 
-app.get('/',(req,res)=>{
+app.get('/', (req,res)=>{
     res.send("welcome to my amnunition Center")
 })
 
-app.get('/amn',(req,res)=>{
-    res.send("welcome to my amnunition Center")
+app.get('/amn',async (req,res)=>{
+    try{
+            //get the data from file
+    const data = JSON.parse (await fs.readFile('./data.json','utf-8'))
+    //sent it to the client
+    res.json(data)
+    } catch(err){
+        res.status(500).json({
+            err:true,
+            message: err
+        })
+    }
+
 })
 
 app.get('/amn/:id',(req,res)=>{
